5878e0862bc9337501782a8b07e0c672a0d08320
unknown <udayyrana>
2024-02-13 22:48:32 +0530

    style: some ui changes

--- src/compnents/General/Body.js
+++ src/compnents/General/Body.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import LeftComponent from '../BodyComponents/LeftComponent';
-import MiddleComponent from '../BodyComponents/MiddleComponent/MiddleComponent';
-import RightComponent from '../BodyComponents/RightComponent';
-import './Body.css'
+import React from "react";
+import LeftComponent from "../BodyComponents/LeftComponent";
+import MiddleComponent from "../BodyComponents/MiddleComponent/MiddleComponent";
+import RightComponent from "../BodyComponents/RightComponent";
+import "./Body.css";
 
 const Body = () => {
   return (
-    <div className='mainBody'>
-      <LeftComponent/>
-      <MiddleComponent/>
-      <RightComponent/>
+    <div className="mainBody">
+      <LeftComponent />
+      <MiddleComponent />
+      {/* <RightComponent/> */}
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
