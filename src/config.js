2b7b276ac4f050ec75f2594ae4c8fb273684fe3f
unknown <udayyrana>
2024-02-13 19:55:24 +0530

    style: some changes in ui

--- src/config.js
+++ src/config.js
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-console.log(process.env.REACT_APP_STORAGE_BUCKET);
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
